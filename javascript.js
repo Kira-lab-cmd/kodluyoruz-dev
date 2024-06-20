7d10679f3ac5ccfbac091d90aef5e9b910b442ba
yusuf tetik <muratyusuf.mt@gmail.com>
2024-06-20 17:33:47 +0300

    son

--- javascript.js
+++ javascript.js
@@ -1,14 +1,11 @@
 let adSoyad = (prompt("Adınızı ve soyadınızı girinz"))
 let isim = document.querySelector("#isim")
-isim.classList.add("text-primary")
 isim.innerHTML = `Merhaba, ${ adSoyad } ! Hoş Geldin!`
 
 let baslik = document.querySelector("#kodluyoruz")
-baslik.classList.add("text-primary")
 baslik.innerHTML = `KODLUYORUZ`
 
 let tarih = document.querySelector("#saat")
-tarih.classList.add("text-primary")
 const gun = ["Pazartesi","Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi","Pazar"];
 function guncelSaniye(){
     let date = new Date()
