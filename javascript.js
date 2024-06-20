94a2f3c34577deb942c914aa9104b953b5de51dd
yusuf tetik <muratyusuf.mt@gmail.com>
2024-06-20 17:26:23 +0300

    güncellenen zaman

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
-let adSoyad = prompt("Adınızı ve soyadınızı girinz")
+let adSoyad = (prompt("Adınızı ve soyadınızı girinz"))
+let isim = document.querySelector("#isim")
+isim.classList.add("text-primary")
+isim.innerHTML = `Merhaba, ${ adSoyad } ! Hoş Geldin!`
 
 let baslik = document.querySelector("#kodluyoruz")
-baslik.classList.add(text-primary)
-let isim = document.querySelector("isim")
-isim.innerHTML = `Merhaba, ${adSoyad}! Hoş Geldin!`
+baslik.classList.add("text-primary")
+baslik.innerHTML = `KODLUYORUZ`
 
-let tarih = document.querySelector("saat")
-const date = new Date() 
-tarih.innerHTML = `${ date.getDay }`
+let tarih = document.querySelector("#saat")
+tarih.classList.add("text-primary")
+const gun = ["Pazartesi","Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi","Pazar"];
+function guncelSaniye(){
+    let date = new Date()
+    tarih.innerHTML = `${ date.getHours() }:${ date.getMinutes() }:${ date.getSeconds() }  
+    ${ gun[date.getDay() - 1 ] }`
+}
+guncelSaniye();
+setInterval(guncelSaniye, 1000);
